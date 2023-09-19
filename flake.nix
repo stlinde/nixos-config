a051b2f0359da3390b7a780c13959c6cfaf05d02
Sebastian Hempel Linde <sebastian@tved-linde.dk>
2023-09-19 11:52:56 +0200

    Working nvim config

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Your new nix config";
 
+  # Inputs are a bit like nixpkgs
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
@@ -13,12 +14,6 @@
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = {
@@ -26,35 +21,43 @@
     nixpkgs,
     home-manager,
     ...
-  } @ inputs: let
+  } @ inputs:
+  let
     inherit (self) outputs;
+
     # Supported systems for your flake packages, shell, etc.
     systems = [
       "x86_64-linux"
     ];
+
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
     forAllSystems = nixpkgs.lib.genAttrs systems;
+
   in {
     # Your custom packages
     # Acessible through 'nix build', 'nix shell', etc
     packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
+
     # Reusable nixos modules you might want to export
     # These are usually stuff you would upstream into nixpkgs
     nixosModules = import ./modules/nixos;
+
     # Reusable home-manager modules you might want to export
     # These are usually stuff you would upstream into home-manager
     homeManagerModules = import ./modules/home-manager;
 
     # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
+    # Available through 'nixos-rebuild --flake .#nixos'
     nixosConfigurations = {
+      # nixos is going to be the hostname
       nixos = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
@@ -63,8 +66,5 @@
         ];
       };
     };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
   };
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,8 +15,7 @@
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
+# You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
   ];
 
@@ -47,14 +46,12 @@
     };
   };
 
-  # TODO: Set your username
   home = {
     username = "slinde";
     homeDirectory = "/home/slinde";
   };
 
   # Add stuff for your user as you see fit:
-  programs.neovim.enable = true;
   home.packages = with pkgs; [
     # Browser
     firefox
@@ -77,8 +74,145 @@
     userEmail = "sebastian@tved-linde.dk";
   };
 
+
+  # Neovim
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withPython3 = true;
+    extraLuaConfig = ''
+      local g = vim.g        -- Global variables
+      local opt = vim.opt    -- Set options
+      local augroup = vim.api.nvim_create_augroup
+      local autocmd = vim.api.nvim_create_autocmd
+
+      -----------------------------------------------------------
+      -- General
+      -----------------------------------------------------------
+      opt.mouse = 'a'                       -- Enable mouse support
+      opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
+      opt.swapfile = false                  -- Don't use swapfile
+      opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
+
+      -----------------------------------------------------------
+      -- Neovim UI
+      -----------------------------------------------------------
+      opt.number = true           -- Show line number
+      opt.showmatch = true        -- Highlight matching parenthesis
+      opt.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
+      opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
+      opt.splitright = true       -- Vertical split to the right
+      opt.splitbelow = true       -- Horizontal split to the bottom
+      opt.ignorecase = true       -- Ignore case letters when search
+      opt.smartcase = true        -- Ignore lowercase for the whole pattern
+      opt.linebreak = true        -- Wrap on word boundary
+      opt.laststatus=3            -- Set global statusline
+      
+      -----------------------------------------------------------
+      -- Tabs, indent
+      -----------------------------------------------------------
+      opt.expandtab = true        -- Use spaces instead of tabs
+      opt.shiftwidth = 4          -- Shift 4 spaces when tab
+      opt.tabstop = 4             -- 1 tab == 4 spaces
+      opt.smartindent = true      -- Autoindent new lines
+      
+      -----------------------------------------------------------
+      -- Memory, CPU
+      -----------------------------------------------------------
+      opt.hidden = true           -- Enable background buffers
+      opt.history = 100           -- Remember N lines in history
+      opt.lazyredraw = true       -- Faster scrolling
+      opt.synmaxcol = 240         -- Max column for syntax highlight
+      opt.updatetime = 250        -- ms to wait for trigger an event
+      
+      -----------------------------------------------------------
+      -- Startup
+      -----------------------------------------------------------
+      -- Disable nvim intro
+      opt.shortmess:append "sI"
+      
+      -- -- Disable builtin plugins
+      local disabled_built_ins = {
+         "2html_plugin",
+         "getscript",
+         "getscriptPlugin",
+         "gzip",
+         "logipat",
+         "matchit",
+         "tar",
+         "tarPlugin",
+         "rrhelper",
+         "spellfile_plugin",
+         "vimball",
+         "vimballPlugin",
+         "zip",
+         "zipPlugin",
+         "tutor",
+         "rplugin",
+         "synmenu",
+         "optwin",
+         "compiler",
+         "bugreport",
+         "ftplugin",
+      }
+      
+      for _, plugin in pairs(disabled_built_ins) do
+         g["loaded_" .. plugin] = 1
+      end
+
+      augroup('setIndent', { clear = true })
+      autocmd('Filetype', {
+        group = 'setIndent',
+        pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
+          'yaml', 'lua'
+        },
+        command = 'setlocal shiftwidth=2 tabstop=2'
+      })
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      plenary-nvim
+      nvim-lspconfig
+      nvim-treesitter.withAllGrammars
+      mason-nvim
+      mason-lspconfig-nvim
+      nvim-cmp
+      cmp-nvim-lsp
+      luasnip
+      {
+          plugin = lsp-zero-nvim;
+          type = "lua";
+          config = ''
+          local lsp = require('lsp-zero').preset({})
+
+          lsp.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings
+            -- to learn the available actions
+            lsp.default_keymaps({buffer = bufnr})
+          end)
+      
+          lsp.setup()
+          '';
+      }
+      {
+          plugin = tokyonight-nvim;
+          type = "lua";
+          config = ''
+          vim.cmd("colorscheme tokyonight-night")
+          '';
+      }
+      mini-nvim
+    ];
+    extraPackages = with pkgs; [
+      cargo
+    ];
+  };
+
   # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+  # system.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
